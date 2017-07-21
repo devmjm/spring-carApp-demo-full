d21a0af76f224ca35dc10cffd76a0f2bb5088082
devmjm <devmjm@users.noreply.github.com>
2017-07-21 09:59:10 -0700

    First commit

---
+++ CarApp/src/main/java/com/daimler/CarApp/model/Car.java
@@ -0,0 +1,19 @@
+package com.daimler.CarApp.model;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Car {
+	
+	@Id
+	@NotNull
+	public int Vin;
+	
+	@NotNull
+	public UUID Driver;
+
+}
